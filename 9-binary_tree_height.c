acaf0d1527cdef96e645ab879b1b6122284c652d
Khareemah <ajimobikareemah@gmail.com>
2022-08-25 06:50:39 -0700

    height of a tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - find the height of a tree
+* @tree: pointer to node of tree
+* Return: height of the tree or 0
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree){
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	else
+		left_height = 0;
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
+	else
+		right_height = 0;
+	if (right_height > left_height)
+		return (right_height);
+	else
+		return (left_height);
+}
