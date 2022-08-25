aa1a37aef8dfa931988c32cb24bfbe4df7796abf
Khareemah <ajimobikareemah@gmail.com>
2022-08-25 07:19:01 -0700

    depth of a treee

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,12 +7,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t count = 0;
 
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
-	if (tree->parent)
-	{
-		count += 1;
-		binary_tree_depth(tree->parent);
-	}
+	count = binary_tree_depth(tree->parent);
+	count += 1;
 	return (count);
 }
