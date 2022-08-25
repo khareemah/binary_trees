40190cd242c12aeeaf637d012e8f32c114565536
Khareemah <ajimobikareemah@gmail.com>
2022-08-25 08:33:28 -0700

    fix betty error

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_size - measure size of a binary tree
+* binary_tree_size - Measures the size of a binary tree
 * @tree: pointer to the root node
-* Return : 0 or size of a binary tree
+* Return: 0 or size of a binary tree
 */
 
 size_t binary_tree_size(const binary_tree_t *tree)
