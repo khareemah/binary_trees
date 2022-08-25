115e4525a30b194cb26a64051f30d5aee90e3409
Khareemah <ajimobikareemah@gmail.com>
2022-08-25 04:12:53 -0700

    delete all nodes in a binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_delete - deletes all odes in binary tree
+* tree: pointer to the root node
+* Return: Nothing
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
