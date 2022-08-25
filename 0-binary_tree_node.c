75fb87fae3b8bea77cd15730647c286f4893f95a
Khareemah <ajimobikareemah@gmail.com>
2022-08-25 03:20:13 -0700

    fix betty errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,14 +1,22 @@
 #include "binary_trees.h"
+/**
+* binary_tree_node - creates a binary tree
+* @parent: pointer to the parent of the node created
+* @value: value of the node created
+* Return: pointer to newnode or NULL
+**/
+#include "binary_trees.h"
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 	 binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
 
 	if (newnode == NULL)
-		return NULL;
+		return  (NULL);
 	newnode->parent = parent;
 	newnode->n = value;
 	newnode->left = NULL;
 	newnode->right = NULL;
 
-	return newnode;
+	return (newnode);
 }
